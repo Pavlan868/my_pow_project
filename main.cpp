7e36492eda29261268aab694d111f745fec00bb1
Pavlan868 <pavlik.temnikov.044@gmail.com>
2025-11-18 15:33:22 +0700

    add fast pow algorithm

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+// main.cpp
 #include <iostream>
 
 double my_pow(double base, unsigned int exp) {
+    if (exp == 0) return 1.0;
     double result = 1.0;
-    for (unsigned int i = 0; i < exp; ++i) {
-        result *= base;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result *= base;
+        }
+        base *= base;
+        exp /= 2;
     }
     return result;
 }
